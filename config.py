c3df4a55a31848960d13345787228752433fa142
Gibran <gibranhl2@gmail.com>
2021-05-05 19:27:28 -0500

    update uri known issue postgres

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 class Config(object):
-    DATABASE_URI = 'postgres://yyomyofxhndwkx:42c05705f098442101fa4f9a3490a71ad72d26dfd784063972a1ee49a2f2b675@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d24nk8chisovpo'
+    DATABASE_URI = 'postgresql://yyomyofxhndwkx:42c05705f098442101fa4f9a3490a71ad72d26dfd784063972a1ee49a2f2b675@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d24nk8chisovpo'
