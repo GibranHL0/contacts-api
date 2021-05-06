ba2e0382bdfd2d05e3bed27e6ae040e10a4a1452
Gibran <gibranhl2@gmail.com>
2021-05-05 19:14:27 -0500

    update uri

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 class Config(object):
-    DATABASE_URI = 'postgresql://avclytlfaondze:dc6dde67d51e723244f932061728e7b9905b2c0aff20465e3cd54f0466df38ea@ec2-3-233-7-12.compute-1.amazonaws.com:5432/dblro19vhr293t'
+    DATABASE_URI = 'postgres://yyomyofxhndwkx:42c05705f098442101fa4f9a3490a71ad72d26dfd784063972a1ee49a2f2b675@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d24nk8chisovpo'
