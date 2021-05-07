5a6debd6081a1af898069ab4baf5473262ca7432
Gibran <gibranhl2@gmail.com>
2021-05-07 10:27:42 -0500

    env added

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
+import os
+
 class Config(object):
-    DATABASE_URI = 'postgresql://yyomyofxhndwkx:42c05705f098442101fa4f9a3490a71ad72d26dfd784063972a1ee49a2f2b675@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d24nk8chisovpo'
+    raw_uri = os.getenv('DATABASE_URL')
+    DATABASE_URI = raw_uri[:7] + 'ql' + raw_uri[7:]
