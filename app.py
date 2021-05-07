e8f9af3f60cc3101466c0e3ee325dc2759a0e05d
Gibran <gibranhl2@gmail.com>
2021-05-07 10:39:04 -0500

    deploy final version

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from re import search
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins":"*"}})
-print(Config.DATABASE_URI)
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
