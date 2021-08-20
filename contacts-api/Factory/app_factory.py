451275e4925aa37329aea79ea32407f871e3dea2
GibranHL0 <gibranhl2@gmail.com>
2021-08-20 15:48:29 -0500

    Added logging

--- contacts-api/Factory/app_factory.py
+++ contacts-api/Factory/app_factory.py
@@ -4,6 +4,7 @@ from config import Config
 from Factory.factory import Factory
 from flask import Flask
 from flask_cors import CORS
+import logging
 
 
 class AppFactory(Factory):
@@ -21,12 +22,21 @@ class AppFactory(Factory):
             Flask instance.
         """
         self.apply_cors()
+        self.apply_logging()
         self.apply_config()
         return self.app
 
     def apply_cors(self) -> None:
         """Apply CORS policy into the app."""
         CORS(self.app, resources={'/*': {'origins': '*'}})
+
+    def apply_logging(self) -> None:
+        """Apply Logging policy into the app."""
+        logging.basicConfig(
+            filename='../record.log',
+            level=logging.DEBUG,
+            format=f'%(levelname)s %(name)s %(threadName)s : %(message)s',
+        )
 
     def apply_config(self) -> None:
         """Apply the configuration into the app."""
