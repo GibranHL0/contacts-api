2abba15b21dd989c08ac4c42bef91b64b72806bc
GibranHL0 <gibranhl2@gmail.com>
2021-08-24 15:32:21 -0500

    Catch exceptions and convert them into defined errors

--- contacts-api/Services/contact_service.py
+++ contacts-api/Services/contact_service.py
@@ -1,11 +1,11 @@
 """Provides all the logic behind the endpoint's operations."""
 
-from new_app import app
+
 from Errors import errors
 from Factory.contact_factory import ContactFactory
-from Logic import validation
+from Validation import validation
 from Models.contacts import Contact
-from new_app import db
+from new_app import db, app
 from Repository.repository import SQLAlchemyRepository
 from typing import List
 
@@ -22,13 +22,12 @@ def create_contact(contact_dict: dict) -> Contact:
 
     Raises:
         EmailAlreadyExists: Indicate that the email is not unique.
+        InternalError: Indicate that something unexpected happened.
     """
     repo = SQLAlchemyRepository(db.session)
 
     # Validate if the contact email exists, raise an exception if so.
-    exists = validation.check_contact_exist(contact_dict.get('email'), repo)
-
-    if exists:
+    if validation.check_contact_exist(contact_dict.get('email'), repo):
         raise errors.EmailAlreadyExists
 
     # Validate the contact information. Otherwise raise an exception.
@@ -36,7 +35,13 @@ def create_contact(contact_dict: dict) -> Contact:
 
     factory = ContactFactory()
     new_contact = factory.create(contact_dict)
-    repo.addcontact(new_contact)
+
+    try:
+        repo.addcontact(new_contact)
+
+    except Exception as error:
+        app.logger.error(error)
+        raise errors.InternalError
 
     return new_contact
 
@@ -54,20 +59,24 @@ def update_contact(email: str, contact: dict) -> Contact:
 
     Raises:
         EmailNotFound: The email is not in the DB and can't be updated.
+        InternalError: Indicate that something unexpected happened.
     """
     repo = SQLAlchemyRepository(db.session)
 
     # Validate the contact information. Otherwise raise an exception.
     validation.validate_contact(contact)
 
-    # Validate if the contact email exists, raise an exception if so.
-    exists = validation.check_contact_exist(contact.get('email'), repo)
-
-    if not exists:
+    # Validate if the contact email exists, raise an exception if don't.
+    if not validation.check_contact_exist(contact.get('email'), repo):
         raise errors.EmailNotFound
 
     # Updates the contact.
-    repo.updatecontact(email, contact)
+    try:
+        repo.updatecontact(email, contact)
+
+    except Exception as error:
+        app.logger.error(error)
+        raise errors.InternalError
 
     # Creates the factory to create the contact object for the response.
     contact_factory = ContactFactory()
@@ -84,17 +93,21 @@ def delete_contact(email: str) -> None:
 
     Raises:
         EmailNotFound: When the contact is not in the db
+        InternalError: Indicate that something unexpected happened.
     """
     repo = SQLAlchemyRepository(db.session)
 
     # Verify if the email exists.
-    exists = validation.check_contact_exist(email, repo)
-
-    if not exists:
+    if not validation.check_contact_exist(email, repo):
         raise errors.EmailNotFound
 
     # Delete the contact
-    repo.deletecontact(email)
+    try:
+        repo.deletecontact(email)
+
+    except Exception as error:
+        app.logger.error(error)
+        raise errors.InternalError
 
 
 def obtain_contacts(quantity: int) -> List[Contact]:
@@ -106,8 +119,16 @@ def obtain_contacts(quantity: int) -> List[Contact]:
 
     Returns:
         List of Contacts.
+
+    Raises:
+        InternalError: Indicate that something unexpected happened.
     """
-    contacts_data = SQLAlchemyRepository(db.session).getcontacts(quantity)
+    try:
+        contacts_data = SQLAlchemyRepository(db.session).getcontacts(quantity)
+    except Exception as error:
+        app.logger.error(error)
+        raise errors.InternalError
+
     contacts_list = []
 
     if contacts_data:
